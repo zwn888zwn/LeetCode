f47c4817cee7168eae6d19c61e2dda8d7aa729b4
zwn888zwn <zwn888zwn@vip.qq.com>
2019-12-05 20:54:32 +0800

    add

--- src/Trie.java
+++ src/Trie.java
@@ -1,39 +1,71 @@
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Trie {
     private class TrieStru{
-        char var;
-        boolean end;
-        Queue<TrieStru> next=new LinkedList<>();
-        public TrieStru(char var){
-            this.var=var;
+        private boolean isEnd;
+        private TrieStru[] content=new TrieStru[26];
+
+        public void setEnd(){
+            this.isEnd=true;
         }
+        public boolean getEnd(){
+            return isEnd;
+        }
+        public TrieStru getTrie(char c){
+            return content[c-'a'];
+        }
+        public TrieStru setTrie(char c){
+            return content[c-'a']=new TrieStru();
+        }
+
     }
 
     TrieStru root;
     /** Initialize your data structure here. */
     public Trie() {
-        root=new TrieStru('-');
+        root=new TrieStru();
     }
 
     /** Inserts a word into the trie. */
     public void insert(String word) {
         TrieStru p=root;
-        for(int i=0;i<word.length();i++){
-            if (Collections.binarySearch(root.next,word))
+        for (int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            if(p.getTrie(word.charAt(i))==null){
+                p=p.setTrie(c);
+            }else
+                p=p.getTrie(c);
+            if (i==word.length()-1){
+                p.setEnd();
+            }
+
         }
+
     }
 
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-
+        TrieStru p=root;
+        for (int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            p=p.getTrie(c);
+            if (p==null)
+                return false;
+        }
+        if (p.getEnd())
+            return true;
+        return false;
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-
+        TrieStru p=root;
+        for (int i=0;i<prefix.length();i++){
+            char c=prefix.charAt(i);
+            p=p.getTrie(c);
+            if (p==null)
+                return false;
+        }
+        return true;
     }
 }
 
